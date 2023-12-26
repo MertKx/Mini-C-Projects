d72a2575588774f659e94be187955689b9f2d447
Cafer Mert Kalay <113070286+MertKx@users.noreply.github.com>
2023-12-26 11:16:14 +0300

    Add files via upload

---
+++ showNumber.c
@@ -0,0 +1,12 @@
+/*this program shows number that entered by user */
+#include <stdio.h>
+
+int main(void) {
+	int number;
+	printf("Enter a number: ");
+	scanf("%d" , &number);
+	
+	printf("Entered number: %d" , number);
+	
+	return 0;
+}
