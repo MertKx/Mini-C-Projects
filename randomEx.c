743a52ba5c5604ab4847de55b56a5c20e220b26c
Cafer Mert Kalay <113070286+MertKx@users.noreply.github.com>
2023-12-27 12:11:54 +0300

    Add files via upload

---
+++ randomEx.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int studentNumber , num1 , result3 , visa_result;
+	
+	int a;
+	printf("Please enter a value: ");
+	scanf("%d" , &a);
+	
+	printf("This is a C program. \n");
+	
+	printf("This is a \n");
+	printf("C program. \n");
+	
+	printf("This\t is\t a\t C\t program.");
+	
+	
+}
