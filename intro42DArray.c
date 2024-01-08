f378d6ce102d60e9001e243e1807038bae9d4d66
Cafer Mert Kalay <113070286+MertKx@users.noreply.github.com>
2024-01-08 23:36:55 +0300

    Add files via upload

---
+++ intro42DArray.c
@@ -0,0 +1,16 @@
+// this program shows all the elements in spesific two diamensional array
+
+int main(void) {
+	int a[2][3] = {{1,2,3} , {4,5,6}};
+	int i,j;
+	
+	for(i=0;i<2;i++){
+		for(j=0;j<3;j++){
+			printf("%d" , a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+	
+}
