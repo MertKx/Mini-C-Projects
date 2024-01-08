156329b893200436f184b9df5aa546ec50a63e45
Cafer Mert Kalay <113070286+MertKx@users.noreply.github.com>
2024-01-08 23:37:13 +0300

    Add files via upload

---
+++ columnAndRowCalculation.c
@@ -0,0 +1,39 @@
+//finding sum of columns and rows
+
+#include <stdio.h>
+
+int main(void){
+	int array[5][5] = {
+		{8,3,9,0,10},
+		{3,5,17,1,1},
+		{2,8,6,23,1},
+		{15,7,3,2,9},
+		{6,14,2,6,0}
+	};
+	int i,j;
+	int sum=0;
+	
+	//rows
+	printf("Row total: ");
+	
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			sum += array[i][j];	
+		}
+		printf(" %d" , sum);
+		sum=0;
+	}
+	
+	//columns
+	printf("\nColumn total: ");
+	
+	for(j=0;j<5;j++){
+		for(i=0;i<5;i++){
+			sum += array[i][j];
+		}
+		printf(" %d" , sum); 
+		sum=0;
+	}
+	return 0;
+	
+}
