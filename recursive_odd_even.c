9bc33e247c3c0a19c9e513577500fcffbd382df9
Cafer Mert Kalay <113070286+MertKx@users.noreply.github.com>
2024-04-08 14:32:49 +0300

    Add files via upload

---
+++ recursive_odd_even.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int n=1;
+void odd(){
+    if(n<=10){
+        printf("%d ",n+1);
+        n++;
+        even();
+    }
+    return;
+}
+void even(){
+    if(n<=10){
+        printf("%d ",n-1);
+        n++;
+        odd();
+    }
+    return;
+}
+
+
+int main(){
+    odd();
+    return 0;
+}
